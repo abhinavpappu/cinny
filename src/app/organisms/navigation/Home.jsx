90d810e29dcda67a6a4c0fb45e3e00adef36e6ea
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-11-25 03:24:07 -0600

    Re-sort list (by most recent message) when new message sent

--- src/app/organisms/navigation/Home.jsx
+++ src/app/organisms/navigation/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import initMatrix from '../../../client/initMatrix';
@@ -15,16 +15,18 @@ import { AtoZ, mostRecentlyMessaged } from './common';
 const drawerPostie = new Postie();
 function Home({ spaceId }) {
   const [, forceUpdate] = useState({});
-  const { roomList, notifications } = initMatrix;
+  const { roomList, notifications, roomsInput } = initMatrix;
   let spaceIds = [];
   let roomIds = [];
-  let directIds = [];
+
+  // using a ref so that messageSentInRoom can access current value (see https://dmitripavlutin.com/react-hooks-stale-closures/#31-useeffect)
+  const directIds = useRef([]);
 
   const spaceChildIds = roomList.getSpaceChildren(spaceId);
   if (spaceChildIds) {
     spaceIds = spaceChildIds.filter((roomId) => roomList.spaces.has(roomId)).sort(AtoZ);
     roomIds = spaceChildIds.filter((roomId) => roomList.rooms.has(roomId)).sort(AtoZ);
-    directIds = spaceChildIds.filter((roomId) => roomList.directs.has(roomId)).sort(AtoZ);
+    directIds.current = spaceChildIds.filter((roomId) => roomList.directs.has(roomId)).sort(AtoZ);
   } else {
     spaceIds = [...roomList.spaces]
       .filter((roomId) => !roomList.roomIdToParents.has(roomId)).sort(AtoZ);
@@ -34,7 +36,7 @@ function Home({ spaceId }) {
 
   // TODO: temporary, remove later: combines all rooms and DMs into single list
   // (using directIds list since this list is displayed with profile pictures in sidebar)
-  directIds = [...roomIds, ...roomList.directs].sort(mostRecentlyMessaged);
+  directIds.current = [...roomIds, ...roomList.directs].sort(mostRecentlyMessaged);
   roomIds = [];
 
   function selectorChanged(selectedRoomId, prevSelectedRoomId) {
@@ -45,11 +47,18 @@ function Home({ spaceId }) {
     if (addresses.length === 0) return;
     drawerPostie.post('selector-change', addresses, selectedRoomId);
   }
+  function messageSentInRoom(roomId) {
+    // if room not already at top, re-sort list (since it's now room with most recent message)
+    if (directIds.current[0] !== roomId) {
+      forceUpdate({});
+    }
+  }
   function notiChanged(roomId, total, prevTotal) {
     if (total === prevTotal) return;
     if (drawerPostie.hasTopicAndSubscriber('unread-change', roomId)) {
       drawerPostie.post('unread-change', roomId);
     }
+    messageSentInRoom(roomId);
   }
 
   function roomListUpdated() {
@@ -68,10 +77,12 @@ function Home({ spaceId }) {
     roomList.on(cons.events.roomList.ROOMLIST_UPDATED, roomListUpdated);
     navigation.on(cons.events.navigation.ROOM_SELECTED, selectorChanged);
     notifications.on(cons.events.notifications.NOTI_CHANGED, notiChanged);
+    roomsInput.on(cons.events.roomsInput.MESSAGE_SENT, messageSentInRoom);
     return () => {
       roomList.removeListener(cons.events.roomList.ROOMLIST_UPDATED, roomListUpdated);
       navigation.removeListener(cons.events.navigation.ROOM_SELECTED, selectorChanged);
       notifications.removeListener(cons.events.notifications.NOTI_CHANGED, notiChanged);
+      roomsInput.removeListener(cons.events.roomsInput.MESSAGE_SENT, messageSentInRoom);
     };
   }, []);
 
@@ -99,8 +110,8 @@ function Home({ spaceId }) {
         />
       )) }
 
-      { directIds.length !== 0 && <Text className="cat-header" variant="b3">People</Text> }
-      { directIds.map((id) => (
+      { directIds.current.length !== 0 && <Text className="cat-header" variant="b3">People</Text> }
+      { directIds.current.map((id) => (
         <Selector
           key={id}
           roomId={id}
